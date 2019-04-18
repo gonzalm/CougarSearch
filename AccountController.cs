6c9a5a322e4bde0215388ffdc5f526a6c59f6fad
millsja3 <48810919+millsja3@users.noreply.github.com>
2019-04-18 16:21:15 -0400

    Add files via upload

---
+++ AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Login_MVC_.Models;
+using System.Data.SqlClient;
+namespace Login_MVC_.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "data source = (localdb)/MSSQLLocalDB; database = Cougar_Search; integrated sercruity = SSPI;";
+        }
+        [HttpPost]
+        public ActionResult Verfiy(Account ac)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from tbl_Users where userName = '"+ac.userName+"' and password = '"+ac.Password+"' ";
+            dr = com.ExecuteReader();
+            if(dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else{
+                con.Close();
+                return View("Error");
+            }
+
+
+            
+
+        }
+    }
+}
