dbc46edd774572d9c4ca2093481996e4fc40f151
gonzalm <gonzalez.m331@gmail.com>
2019-04-18 23:42:39 -0400

    added experimental folder for possible php work

---
+++ EXPERIMENTAL/Users.sql
@@ -0,0 +1,5 @@
+insert into Users (userName, password, email, phoneNumber) values ('egeorgel0', 'mID2ttV', 'rstroyan0@ycombinator.com', '(332) 4517825');
+insert into Users (userName, password, email, phoneNumber) values ('gcornejo1', 'li6KkZAwWYL', 'lguiet1@tmall.com', '(457) 6521389');
+insert into Users (userName, password, email, phoneNumber) values ('gblaymires2', 'FIyasgvee', 'rclaye2@histats.com', '(656) 9956490');
+insert into Users (userName, password, email, phoneNumber) values ('tpoppleston3', 'geHoTix6Tbd', 'mbrackstone3@ted.com', '(140) 8036082');
+insert into Users (userName, password, email, phoneNumber) values ('mweildish4', 'VwnXTAG', 'wounsworth4@uiuc.edu', '(418) 3225090');
